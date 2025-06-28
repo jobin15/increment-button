1ff98f0e743d90d873e45cf39cc8b18463e9f97a
jobin15 <jobinjoseph1510@gmail.com>
2025-06-28 16:50:16 +0530

    add code

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div><h1>increment button</h1>
+    <MyCounter />
+  </div>
+</template>
+
+<script setup>
+import MyCounter from './components/MyCounter.vue';
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button @click="$emit('increment')">Click to increment value</button>
+  </div>
+</template>
+
+
+<script setup>
+</script>
+
+<style>
+button {
+  padding: 8px;
+  background: red;
+  color: white;
+  border-radius: 40px;
+}
+</style>
